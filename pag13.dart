4089848293a46c30ad60d53b482a0130b6e89d06
IGOR SILVA DOS ANJOS <igor.4971@aluno.pr.senac.br>
2023-04-10 10:33:25 -0300

    atualization

---
+++ pag13.dart
@@ -0,0 +1,11 @@
+void main() {
+  jedi("Tiago", "Dieimes");
+  jedi("Tiago", "Dieimes");
+  jedi("Tiago", "Dieimes");
+  jedi("Tiago", "Dieimes");
+}
+
+void jedi(String professor, aluno) {
+  print("Professor $professor é um Mestre jedi");
+  print("Professor $aluno é o imperador");
+}
