aeeb752eb07488f1061f3b3874a3d32d82c4cb18
IGOR SILVA DOS ANJOS <igor.4971@aluno.pr.senac.br>
2023-05-09 16:22:09 -0300

    baguio do darti

--- lista.dart
+++ lista.dart
@@ -1,10 +1,12 @@
 void main() {
-  List<String> Melhoresdomundo = ['Messi', 'Neymar', 'MBAPPE'];
+  List<String> Melhoresdomundo = ['Messi', 'Neymar', 'MBAPPE', 'gabigol', 'ronaldinho baiano'];
   Melhoresdomundo.add('SIIUUU');
+  print(Melhoresdomundo);
   Melhoresdomundo.remove('gabigol');
+  print(Melhoresdomundo);
 
-  if (Melhoresdomundo.contains('Neymar')) {
-    print('contem esse jogador');
+  if (Melhoresdomundo.contains('ronaldo')) {
+    print('contem esse jogador  na lista');
   } else {
     print('Nao contem esse jogador na lista');
   }
